c3e04e714d0b7d5baa014c707bfbd78b7f02072f
K_van93 <kevin.vanaerschodt@gmail.com>
2021-02-18 16:00:36 +0100

    allow different methods cors

--- app.js
+++ app.js
@@ -7,9 +7,9 @@ app.use(cors());
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "GET", "PUT", "POST", "DELETE", "OPTIONS");
     next();
-  });
-  
+});  
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 const port = process.env.PORT || 80;
