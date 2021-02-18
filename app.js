5cbbdd228eae23aa2358c3f17c67375612dca9d0
K_van93 <kevin.vanaerschodt@gmail.com>
2021-02-18 10:09:38 +0100

    First app commit

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-import express from 'express';
-const process = process.env;
+const express = require('express');
+
 const app = express();
-const port = process.PORT || 80;
+const port = process.env.PORT || 80;
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
