affe24f912887e9b48e039e7bc91b8e74275e73e
K_van93 <kevin.vanaerschodt@gmail.com>
2021-02-18 10:06:34 +0100

    First app commit

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log("Hello, good morning, world");
+import express from 'express';
+const process = process.env;
+const app = express();
+const port = process.PORT || 80;
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+})
+app.get('/test', (req, res) => {
+    res.send('Test');
+})
+app.get('/list', (req, res) => {
+    let arr = ['Kevin', 'Wesley', 'Wout'];
+    res.send(JSON.stringify(arr));
+})
+
+app.listen(port, () => {
+    console.log(`App Server luistert op poort ${port}`);
+})
